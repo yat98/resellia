a6905066fc970e8153c0b683a3fa3dbfe546916f
yat98 <hidayatchandra08@gmail.com>
2021-11-28 14:55:51 +0800

    Add users seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run()
+	{
+		User::create([
+			'name' => 'Admin',
+			'email' => 'admin@mail.com',
+			'password' => 'secret123',
+			'role' => 'admin',
+		]);
+
+		User::create([
+			'name' => 'Customer',
+			'email' => 'customer@mail.com',
+			'password' => 'secret123',
+			'role' => 'customer',
+		]);
+
+		$this->command->info('Success add 2 users');
+	}
+}
