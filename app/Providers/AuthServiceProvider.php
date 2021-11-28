6ceadfe110598af5be3e2b272dba28babf1d60e8
yat98 <hidayatchandra08@gmail.com>
2021-11-28 15:31:51 +0800

    Add font awesome

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,24 +7,28 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		// 'App\Models\Model' => 'App\Policies\ModelPolicy',
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+	/**
+	 * Register any authentication / authorization services.
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
 
-        //
-    }
+		Gate::define('admin-access', function ($user) {
+			return 'admin' == $user->role;
+		});
+
+		Gate::define('customer-access', function ($user) {
+			return 'customer' == $user->role;
+		});
+	}
 }
